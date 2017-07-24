685fc7f5a3405800593abaf94196a077678162fc
JP Doherty <jpdoherty90@gmail.com>
2017-07-24 11:55:59 -0500

    updated views and urls for login/reg

--- apps/stubhub/urls.py
+++ apps/stubhub/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url, include
-from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    url(r'^', include('apps.stubhub.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^', views.index),
+    url(r'^log_reg', views.log_reg),
+    url(r'^registration_attempt$', views.register),
+    url(r'^login_attempt$', views.login),
+    url(r'^success$', views.success),
+    url(r'^logout$', views.logout),
 ]
--- apps/stubhub/views.py
+++ apps/stubhub/views.py
@@ -1,6 +1,88 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
-# Create your views here.
+from models import User #, Event, Performer, Venue
+
+
+
+#-----------------------------------------------------------------
+#-----------------------------------------------------------------
+
+
+
+def index(request):
+    
+    if request.POST:
+    
+    context = {
+
+    }
+  
+    return render(request, 'stubhub/home.html', context)
+
+
+#-----------------------------------------------------------------
+#-----------------------------------------------------------------
+
+def register(request):
+    
+    errors = User.objects.new_user_validator(request.POST)
+    if len(errors):
+        for tag, error in errors.iteritems():
+            messages.add_message(request, messages.ERROR, errors[tag])
+        return redirect("/")
+    else:
+        first_name =  request.POST['first_name']
+        last_name =  request.POST['last_name']
+        email = request.POST['email']
+        hash1 = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
+ 
+        user = User.objects.create(first_name=name, last_name=last_name, email=email, password_hash=hash1)
+
+        if not user:
+            messages.add_message(request, messages.ERROR, "User email already exists.")
+            return redirect("/")
+        else:
+            request.session['user_id'] = user.id
+            return redirect('/success')
+
+#-----------------------------------------------------------------
+#-----------------------------------------------------------------
+
+def success(request):
+    
+    user = User.objects.get(id=request.session['user_id'])
+
+    context = {
+
+    }
+    
+    return render(request, 'stubhub/home.html', context)
+    
+#-----------------------------------------------------------------
+#-----------------------------------------------------------------
+
+def login(request):
+
+    try:
+        user = User.objects.login_validator(request.POST)
+        request.session['name'] = user.name
+        request.session['email'] = user.email
+        request.session['user_id'] = user.id
+        return redirect('/success')
+    except:
+        messages.add_message(request, messages.ERROR, "Invalid login info.")
+        return redirect("/")
+
+#-----------------------------------------------------------------
+#----------------------------------------------------------------
+
+def logout(request):
+    for sesskey in request.session.keys():
+        del request.session[sesskey]
+    return redirect("/")
+
+#-----------------------------------------------------------------
+#-----------------------------------------------------------------
