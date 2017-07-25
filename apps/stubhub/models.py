db287f955bf993d7385c8138d4f8f2964b934f4e
JP Doherty <jpdoherty90@gmail.com>
2017-07-25 14:53:39 -0500

    added list of available tickets

--- apps/stubhub/models.py
+++ apps/stubhub/models.py
@@ -103,6 +103,7 @@ class Ticket(models.Model):
     event = models.ForeignKey(Event, related_name="tickets")
     seller = models.ForeignKey(User, related_name="sellers")
     buyer = models.ForeignKey(User, related_name="buyers", blank=True, null=True)
+    available = models.BooleanField(default=True)
     seat = models.CharField(max_length = 128)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
