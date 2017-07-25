7c45fb49b1d1d839f9ddd570735196d4f3610d66
JulianAuza <julianauza@gmail.com>
2017-07-24 22:55:39 -0500

    fixed merge conflict

--- apps/stubhub/urls.py
+++ apps/stubhub/urls.py
@@ -10,12 +10,5 @@ urlpatterns = [
     url(r'^registration_attempt$', views.register),
     url(r'^login_attempt$', views.login),
     url(r'^success$', views.success),
-<<<<<<< HEAD
-    url(r'^logout$', views.logout),
-    url(r'^sell/(?P<parameter>\d+)$', views.init_sale),
-    url(r'^sell/(?P<parameter>\d+)/post_tickets$', views.post_tickets),
-    url(r'^ticket_posted$', views.ticket_posted),
-=======
     url(r'^post_tickets$', views.post_tickets),
->>>>>>> 2cf4488e9673167d9cee13f6f240a87e7ccf60d1
 ]
