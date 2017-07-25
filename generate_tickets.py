33da3b4bc521a38b30462f240712eff39ea967c3
Alyssa Nickow <alyssa@nickow.com>
2017-07-25 14:45:57 -0500

    added file to generate random tickets for sale

---
+++ generate_tickets.py
@@ -0,0 +1,16 @@
+import django
+from random import randint
+from autofixture import AutoFixture
+from apps.stubhub.models import *
+
+def get_rand_price():
+    return randint(15,125)
+
+def get_rand_seat():
+    return randint(1,200)
+
+
+
+fixture = AutoFixture(Ticket, field_values = {'price': get_rand_price, 'seat': get_rand_seat, 'buyer':None})
+for x in range (0,100):
+    entry = fixture.create(1)
