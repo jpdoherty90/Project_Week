67a8f6ece4d056a7163f7d9eb0da7b39b79c3df4
JulianAuza <julianauza@gmail.com>
2017-07-28 11:06:21 -0500

    Styled search bar

--- apps/stubhub/static/stubhub/script/script.js
+++ apps/stubhub/static/stubhub/script/script.js
@@ -1,17 +1,5 @@
 $(document).ready(function(){
-   var $grid = $('.grid').isotope({
-  // main isotope options
-  itemSelector: '.grid-item',
-  // set layoutMode
-  layoutMode: 'cellsByRow',
-  // options for cellsByRow layout mode
-  cellsByRow: {
-    columnWidth: 200,
-    rowHeight: 150
-  },
-  // options for masonry layout mode
-  masonry: {
-    columnWidth: '.grid-sizer'
-  }
-})
+ 
+ 
+
 });
